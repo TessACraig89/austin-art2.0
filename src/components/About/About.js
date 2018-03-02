56173a19bd3860fb803ece9b5d0ce4f426276df6
Tess Craig <tessashleycraig89@gmail.com>
2018-03-02 00:49:06 -0600

    styles About

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,10 +8,10 @@ class About extends Component {
     return (
       <div className="about">
           <img className="aboutCollage" src={require("../../static/images/skyline.png")}></img>
-        <div className='aboutTextContainer'>
+        <div className='aboutText'>
           <h1 className="title">About</h1>
-          <p className="aboutP">Austin Street Art is lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          <a href="">About The Developer</a>
+          <p className="aboutP">Austin Street Art is lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. In reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Quis nostrud exercitation ullamco laboris nisi ut aliquip ex.</p>
+          <a href="https://www.linkedin.com/in/tesscraig/"><button className="developer">About The Developer</button></a>
       </div>
       </div>
     );
