8afbdf9696ae4236455e83d41a2263110bbbf18f
Tess Craig <tessashleycraig89@gmail.com>
2018-03-17 13:19:17 -0500

    updates about me paragraph

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -10,7 +10,7 @@ class About extends Component {
           <img className="aboutCollage" src={require("../../static/images/skyline.png")}></img>
         <div className='aboutText'>
           <h1 className="title">About</h1>
-          <p className="aboutP">Austin Street Art is lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. In reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Quis nostrud exercitation ullamco laboris nisi ut aliquip ex.</p>
+          <p className="aboutP">Austin Street Art is for sharing images and locations of the many wonderful graffiti stencils, posters, stickers, and installations in the Austin area. Users can browse art, favorite the pieces they enjoy the most, and create posts. My motivation for developing Austin Street Art was that I want the ability to save and share the amazing art I have seen all over Austin. As someone who has lived in Austin for over 16 years I have had some of my favorite street art be covered up and demolished, but with Austin Street Art we can continue to view and appreciate these pieces regardless of our city’s changes.</p>
           <a href="https://www.linkedin.com/in/tesscraig/"><button className="developer">About The Developer</button></a>
       </div>
       </div>
